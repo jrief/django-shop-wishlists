5fd59a7f825cd1255cffc162415cfd186baec413
Jacob Rief <jacob.rief@gmail.com>
2012-01-21 18:15:56 +0100

    Removed useless import

--- shop_wishlists/templatetags/wishlisttags.py
+++ shop_wishlists/templatetags/wishlisttags.py
@@ -1,10 +1,8 @@
 from django import template
 from shop_wishlists.models import Wishlist
-from shop_wishlists.utils import is_product_on_active_wishlist
 
 
 register = template.Library()
-
 
 @register.inclusion_tag('templatetags/_select_wishlist.html', takes_context=True)
 def select_wishlist(context):
