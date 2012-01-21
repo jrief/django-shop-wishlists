c0e3095dd8c1c51894db16faa834bca5c5da09c2
Jacob Rief <jacob.rief@gmail.com>
2012-01-21 18:18:34 +0100

    Using named handler to dispatch post requests

--- shop_wishlists/urls.py
+++ shop_wishlists/urls.py
@@ -5,6 +5,10 @@ from views import WishlistDetailView
 
 urlpatterns = patterns('',
     url(r'^$', WishlistDetailView.as_view(), name='wishlist'),
-    url(r'^update/$', WishlistDetailView.as_view(),
-        name='wishlist_update'),
-    )
+    url(r'^remove/$', WishlistDetailView.as_view(handler='remove'),
+        name='wishlist_remove_item'),
+    url(r'^rename/$', WishlistDetailView.as_view(handler='rename'),
+        name='wishlist_rename'),
+    url(r'^delete/$', WishlistDetailView.as_view(handler='delete'),
+        name='wishlist_delete'),
+)
--- shop_wishlists/views.py
+++ shop_wishlists/views.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 from shop.views import ShopView, ShopTemplateResponseMixin
 from models import Wishlist
 from forms import get_wishlist_formset
-from utils import get_or_create_wishlist, is_product_on_active_wishlist
+from utils import get_or_create_wishlist, is_product_on_active_wishlist, \
+    create_additional_wishlist, switch_wishlist, rename_active_wishlist, \
+    delete_active_wishlist
 
 
 class ProductDetailViewMixin(object):
@@ -35,20 +39,62 @@ class ProductDetailViewMixin(object):
 class WishlistDetailView(ShopTemplateResponseMixin, ShopView):
     model = Wishlist
     template_name = 'wishlist.html'
-    action = None
+    handler = None
+
+    def dispatch(self, request, *args, **kwargs):
+        """
+        If `handler` is defined use it dispatch request to the right method.
+        """
+        if not self.handler:
+            return super(WishlistDetailView, self).dispatch(request, *args,
+                **kwargs)
+        handler = getattr(self, self.handler)
+        self.request = request
+        self.args = args
+        self.kwargs = kwargs
+        return handler()
 
     def get(self, request, *args, **kwargs):
-        wishlist = get_or_create_wishlist(self.request)
+        if request.GET.has_key('select_wishlist'):
+            if request.GET['select_wishlist']=='add_wishlist':
+                wishlist = create_additional_wishlist(request)
+            else:
+                wishlist_id = int(request.GET['select_wishlist'])
+                wishlist = switch_wishlist(request, wishlist_id)
+        else:
+            wishlist = get_or_create_wishlist(self.request)
         context = {}
         context.update({ 'wishlist_name': wishlist.name })
         context.update({ 'wishlist_date_created': wishlist.date_created })
         context.update({ 'wishlist_last_updated': wishlist.last_updated })
         formset = get_wishlist_formset(wishlist_items=wishlist.get_all_items())
-        context.update({'formset': formset, })
+        context.update({ 'formset': formset, })
         return self.render_to_response(context)
 
-    def post(self, request, *args, **kwargs):
-        print "post TODO"
+    def remove(self):
+        """
+        Remove selected item from wishlist
+        """
+        wishlist = get_or_create_wishlist(self.request)
+        wishlist.delete_item(self.request.POST['item_id'])
+        return self.success()
 
-    def delete(self, request, *args, **kwargs):
-        print "post TODO"
+    def rename(self):
+        """
+        Remove active wishlist
+        """
+        rename_active_wishlist(self.request, self.request.POST['wishlist_name'])
+        return self.success()
+
+    def delete(self):
+        """
+        Delete active wishlist
+        """
+        delete_active_wishlist(self.request)
+        return self.success()
+
+    def success(self):
+        if self.request.is_ajax():
+            return HttpResponse('Ok<br />')
+        else:
+            return HttpResponseRedirect(reverse('wishlist'))
